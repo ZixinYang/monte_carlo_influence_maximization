8a4177d15d1786a8f8dec8579bf1d173e954f56a
ZixinYang <zixinyang92@gmail.com>
2019-05-03 16:29:51 +0800

    class of data

---
+++ DATA.py
@@ -0,0 +1,7 @@
+class Data:
+    def __init__(self, IndegreeDic, OutdegreeDic, InweightDic, OutweightDic, threshold):
+        self.threshold = threshold
+        self.OutdegreeDic = OutdegreeDic
+        self.IndegreeDic = IndegreeDic
+        self.OutweightDic = OutweightDic
+        self.InweightDic = InweightDic
